1754107ffca42d0dc24288bdc542cf930a8fd8c0
Decagon <decagon@Decagons-MacBook-Air.local>
2021-07-27 00:09:25 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,12 @@
+
+package main
+
+import "fmt"
+
+func main() {
+	var nameOne = "Chisom Amadi,"
+	var stack = "golang"
+	var squad = "eight"
+	var year int = 2021
+	fmt.Println("My name is", nameOne, "I am a", stack,"developer a member of squad", squad, "of year", year)
+}
